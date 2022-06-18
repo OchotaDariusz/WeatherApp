c7c960f2c8b79ad1a5c62bec314ef8f6debceeeb
Ochota Dariusz <ochota.dariusz@gmail.com>
2022-06-18 11:13:17 +0200

    Save last searched city to local storage

--- scripts/app.js
+++ scripts/app.js
@@ -52,4 +52,12 @@ cityForm.addEventListener('submit', event => {
     updateCity(city)
         .then(data => updateUI(data))
         .catch(err => console.log(err));
+
+    localStorage.setItem('city', city);
 });
+
+if(localStorage.getItem('city')){
+    updateCity(localStorage.getItem('city'))
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+}
